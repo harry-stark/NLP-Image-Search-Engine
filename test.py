87218b6e6597e57ca78cbfbe6ff60e5ee7b6410c
Harry Stark <harsimrans008@gmail.com>
2021-02-20 18:10:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,10 +5,10 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from engine import models
 from engine import pipeline
 from engine.genrator import generator,querygen,find_match
-#way = input("Image Directory")
+way = input("Image Directory:")
 imgem=None
 filelist=None
-imgem,filelist=generator("/home/harry/Documents/Engine2/images")
+imgem,filelist=generator(way)
 print(len(imgem))
 query=['desert']
 quem=querygen(query)
@@ -16,7 +16,7 @@ rs=find_match(imgem,quem,2)
 print(rs)
 print(filelist)
 print(type(imgem))
-print("                                           profileing             ")
+print("************************profiling********************")
 
 
 end=time.time()
